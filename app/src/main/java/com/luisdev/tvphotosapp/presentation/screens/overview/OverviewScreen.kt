d36201dd874d65bf28d6112764dbbef7d8e30c61
Luis Aguilar <luis.aguilar.4658@gmail.com>
2024-05-20 06:13:20 -0500

    feat: add components, views and the main viewmodel

--- app/src/main/java/com/luisdev/tvphotosapp/presentation/screens/overview/OverviewScreen.kt
+++ app/src/main/java/com/luisdev/tvphotosapp/presentation/screens/overview/OverviewScreen.kt
@@ -1,7 +1,44 @@
 package com.luisdev.tvphotosapp.presentation.screens.overview
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.luisdev.tvphotosapp.presentation.screens.overview.search.Body
+import com.luisdev.tvphotosapp.presentation.screens.overview.search.Header
+import com.luisdev.tvphotosapp.presentation.screens.overview.search.SearchView
 
 @Composable
-fun OverviewScreen() {}
+fun OverviewScreen(
+    overviewScreenViewModel: OverviewScreenViewModel = hiltViewModel()
+) {
+
+
+    LaunchedEffect(true){
+        overviewScreenViewModel.init()
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+
+        //HEADER
+        Header( overviewScreenViewModel)
+
+        //SEARCH INPUT TEXT
+        SearchView(overviewScreenViewModel )
+
+        //BODY
+        Body(overviewScreenViewModel)
+
+
+    }
+
+
+}

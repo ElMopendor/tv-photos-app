65d5bea2b02166f7d96ea1495c96c0b89d62d288
Luis Aguilar <luis.aguilar.4658@gmail.com>
2024-05-20 05:58:47 -0500

    feat: add first screen

--- app/src/main/java/com/luisdev/tvphotosapp/MainActivity.kt
+++ app/src/main/java/com/luisdev/tvphotosapp/MainActivity.kt
@@ -3,25 +3,42 @@ package com.luisdev.tvphotosapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.tv.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.tv.material3.ExperimentalTvMaterial3Api
-import androidx.tv.material3.Surface
+import androidx.tv.material3.LocalContentColor
+import androidx.tv.material3.MaterialTheme
+import com.luisdev.tvphotosapp.presentation.screens.App
 import com.luisdev.tvphotosapp.ui.theme.TvPhotosAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
     @OptIn(ExperimentalTvMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             TvPhotosAppTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.surface)
+                ) {
+                    CompositionLocalProvider(
+                        LocalContentColor provides MaterialTheme.colorScheme.onSurface
+                    ) {
+                        App()
 
+                    }
+                }
             }
         }
     }
 }
-
---
+++ app/src/main/java/com/luisdev/tvphotosapp/presentation/screens/App.kt
@@ -0,0 +1,11 @@
+package com.luisdev.tvphotosapp.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.tv.material3.Surface
+
+@Composable
+fun App(){
+    Surface {
+
+    }
+}
